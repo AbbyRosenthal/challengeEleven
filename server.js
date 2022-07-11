b89b8cae4586de96d29e095ac4167b767bf19edb
Abby Rosenthal <adlundeen@gmail.com>
2022-07-11 14:29:19 -0500

    updated heroku port

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const PORT = process.env.PORT || 3002
 const notes = require('./db/db.json')
 
 const app = express();
@@ -9,6 +10,6 @@ app.get('/api/notes', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(3002, () => {
-    console.log(`API server now on port 3002!`);
+app.listen(PORT, () => {
+    console.log(`API server now on ${PORT}`);
   });
