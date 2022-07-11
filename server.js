b786ed179cf9676dc5106e087bf9fc4818e123ed
Abby Rosenthal <adlundeen@gmail.com>
2022-07-11 14:27:25 -0500

    add heroku

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const notes = require('./db/db.json')
 const app = express();
 
 
-//WORKING!!!!
+//working
 app.get('/api/notes', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(3002, () => {
+    console.log(`API server now on port 3002!`);
   });
