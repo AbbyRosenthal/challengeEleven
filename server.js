dbce027267719147a756c3e49123e7c3571ae644
Abby Rosenthal <adlundeen@gmail.com>
2022-07-13 15:04:37 -0500

    post route working

--- server.js
+++ server.js
@@ -1,13 +1,56 @@
-const express = require('express')
-const PORT = process.env.PORT || 3002
-const notes = require('./db/db.json')
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const notes = require('./db/db.json');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
+
+//parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+//parse incoming json data
+app.use(express.json());
+app.use(express.static('public'));
+
+function createNewTask( body, notesArray) {
+    const newTask = body;
+    console.log(newTask);
+    //put functions main code here
+    //determines if vaue is an array
+    // if it is not an array
+if(!Array.isArray(notesArray))
+notesArray = [];
+
+//if the array is empty
+if(notesArray.length === 0)
+notesArray.push(0);
+
+newTask.id = notesArray[0];
+notesArray[0]++;
+
+notesArray.push(newNote);
+fs.writeFileSync(
+    //writing our notes in the subdirectory
+    path.join(__dirname, './db/db.json'),
+    //saves as JSON (null and 2 keep data formatted -  per this weeks mdoules)
+    JSON.stringify(notesArray, null, 2)
+);
+    //return finished code to post route for response
+    return newTask;
+}
 
 
 //working
 app.get('/api/notes', (req, res) => {
-    res.send('Hello World');
+    let results = notes;
+    res.send(results);
+});
+
+//listens for post requests
+app.post('/api/notes', (req, res) => {
+    //req.body is where incoming content will be
+    console.log(req.body);
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
